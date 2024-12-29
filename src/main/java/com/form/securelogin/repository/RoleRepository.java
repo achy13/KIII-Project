1f7ed5d7e3d3c11e5719b55ba0195684b1abd58a
Anastasija <anastasija.bojkikeva@gmail.com>
2024-12-29 21:45:17 +0100

    Add app

---
+++ src/main/java/com/form/securelogin/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.form.securelogin.repository;
+
+import com.form.securelogin.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+}
